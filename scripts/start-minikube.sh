82ca07dace921e5537ccbca09ab5c4894c81629f
Alexander Chumakin <aliaksandr.chumakin@jetbrains.com>
2023-11-12 17:49:21 +0100

    stabilize minikube auto installation

--- scripts/start-minikube.sh
+++ scripts/start-minikube.sh
@@ -11,6 +11,8 @@ kubectl get deployments
 
 minikube addons enable ingress
 minikube addons enable ingress-dns
+
+kubectl wait --for=condition=ready pod -l "app.kubernetes.io/name=payments-demo-quarkus-app" --timeout 240s
 kubectl apply -f src/main/resources/app-ingress.yaml
 
 sleep 5
